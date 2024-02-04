d819332a02a370d2fa83e7cf0b8b8975a4b6ab93
sndrzt <126686466+sndrzt@users.noreply.github.com>
2024-02-04 10:58:41 +0800

    add gnome-panel-2.2.0.1 source code from shrike src dvd
    Signed-off-by: Sndrz Teo <sndrz@outlook.com>

---
+++ README.md
@@ -0,0 +1,2 @@
+# gnome-panel-2.2.0.1
+gnome-panel-2.2.0.1 source code from shrike src dvd
---
+++ gnome-panel/panel-main.h
@@ -0,0 +1,8 @@
+#ifndef __PANEL_MAIN_H__
+#define __PANEL_MAIN_H__
+
+#include <libgnomeui/gnome-icon-theme.h>
+
+extern GnomeIconTheme *panel_icon_theme;
+
+#endif /* __PANEL_MAIN_H__ */
